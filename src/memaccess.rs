8314081eb888cdb0898ac26d6e87c3e467d6f0c3
weiju <weiju.wu@gmail.com>
2023-10-08 22:45:41 -0700

    added 32 bit accessors

--- src/memaccess.rs
+++ src/memaccess.rs
@@ -37,6 +37,28 @@ impl AddressSpace {
         self.chipmem[addr as usize] = a;
         self.chipmem[(addr + 1) as usize] = b;
     }
+
+    // 32 Bit accessors
+    pub fn u32_at(&mut self, addr: u32) -> u32 {
+        let a: u32 = (self.chipmem[addr as usize]).into();
+        let b: u32 = (self.chipmem[(addr + 1) as usize]).into();
+        let c: u32 = (self.chipmem[(addr + 2) as usize]).into();
+        let d: u32 = (self.chipmem[(addr + 3) as usize]).into();
+        return (a << 24) | (b << 16) | (c << 8) | d;
+    }
+
+    pub fn set_u32_at(&mut self, addr: u32, value: u32) {
+        let a: u8 = (value >> 24) as u8;
+        let b: u8 = ((value >> 16) & 0xff) as u8;
+        let c: u8 = ((value >> 8) & 0xff) as u8;
+        let d: u8 = (value & 0xff) as u8;
+
+        self.chipmem[addr as usize] = a;
+        self.chipmem[(addr + 1) as usize] = b;
+        self.chipmem[(addr + 2) as usize] = c;
+        self.chipmem[(addr + 3) as usize] = d;
+    }
+
 }
 
 // This part only gets compiled in test scenarios
@@ -84,5 +106,22 @@ mod tests {
         let mut addr: AddressSpace = AddressSpace::new();
         addr.set_u16_at(0, 32123);
         assert_eq!(32123, addr.u16_at(0));
+    }
+
+    #[test]
+    fn test_u32_at() {
+        let mut addr: AddressSpace = AddressSpace::new();
+        addr.chipmem[0] = 0;
+        addr.chipmem[1] = 0;
+        addr.chipmem[2] = 0;
+        addr.chipmem[3] = 32;
+        assert_eq!(32, addr.u32_at(0));
+    }
+
+    #[test]
+    fn test_set_u32_at() {
+        let mut addr: AddressSpace = AddressSpace::new();
+        addr.set_u32_at(0, 32123123);
+        assert_eq!(32123123, addr.u32_at(0));
     }
 }
